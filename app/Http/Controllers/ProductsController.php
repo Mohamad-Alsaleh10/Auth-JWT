50f180841da313f97dd0e72e3f7f2c311d86933b
Mohamad Alsaleh <eng.mohamad110@gmail.com>
2024-03-30 10:38:33 -0700

    hi

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Products;
 use Illuminate\Http\Request;
-
+use App\Http\Requests\ProductRequest;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 class ProductsController extends Controller
 {
     /**
@@ -13,59 +15,65 @@ class ProductsController extends Controller
     public function index()
     {
         $products = Products::all();
-        return response()->json($products);
+        return response()->json(
+            [
+                'status' => 'success',
+                'Products' =>$products
+            ]
+        );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'desc' => 'required|string',
-            'price' => 'required|numeric|min:0',
-        ]);
+        try {
+            DB::beginTransaction();
+            $product = Products::create(
+                [
+                    'name' => $request->name,
+                    'desc' => $request->desc,
+                    'price' => $request->price,
+                ]
+            );
 
-        $product = Products::create($validatedData);
-
-        return response()->json($product, 201);
+            DB::commit();
+            return response()->json(
+                [
+                    'status'=>'success',
+                    'product' => $product
+                ]
+            );
+        } catch (\Throwable $th) {
+            DB::rollback();
+            Log::error($th);
+            return response()->json(
+                [
+                    'status'=>'error',
+                    'product' => $product
+                ]
+            );
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show($id)
+    public function show(Products $product)
     {
-        $product = Products::find($id);
-        if ($product) {
-            return response()->json($product);
-        } else {
-            return response()->json(['message' => 'Product not found'], 404);
-        }
 
+        return response()->json(
+            [
+                'status'=>'success',
+                'product'=>$product
+            ]
+            );
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Products $products)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Products $products)
+    public function update(Request $request, Products $product)
     {
         $validatedData = $request->validate([
             'name' => 'sometimes|required|string|max:255',
@@ -73,24 +81,28 @@ class ProductsController extends Controller
             'price' => 'sometimes|required|numeric|min:0',
         ]);
 
-        $products->update($validatedData);
+        $product->update($validatedData);
 
-        return response()->json($products);
+        return response()->json(
+            [
+                'status'=>'success',
+                'product' => $product
+            ]
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy($id)
+    public function destroy(Products $Product)
     {
 
-        $product = Products::find($id);
-        if ($product) {
-            $product->delete();
-            return response()->json('success delete ');
-        } else {
-            return response()->json(['message' => 'Product not found'], 404);
-        }
+        $Product->delete();
+        return response()->json(
+         [
+            'status'=>'success'
+         ]
+        );
 
     }
 }
